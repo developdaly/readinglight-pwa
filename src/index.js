db27b81accacd2abd71263671f3ade671d6345cd
Patrick Daly <patrick@developdaly.com>
2019-02-03 22:34:17 -0600

    Roku changes

--- src/index.js
+++ src/index.js
@@ -1,73 +1,57 @@
 'use strict';
 
+// Instantiate MDC Drawer
+const drawerEl = document.querySelector('.mdc-drawer');
+const drawer = new mdc.drawer.MDCDrawer.attachTo(drawerEl);
+
+// Instantiate MDC Top App Bar (required)
+const topAppBarEl = document.querySelector('.mdc-top-app-bar');
+const topAppBar = new mdc.topAppBar.MDCTopAppBar.attachTo(topAppBarEl);
+
+topAppBar.setScrollTarget(document.querySelector('.main-content'));
+topAppBar.listen('MDCTopAppBar:nav', () => {
+  drawer.open = !drawer.open;
+});
+
+
+mdc.textField.MDCTextField.attachTo(document.querySelector('.mdc-text-field'));
+
+
 /** @const */
 const appOpts = {
   dom: {
     root: document.documentElement,
-    lux: document.querySelector('#lux'),
-    color: document.querySelector('#color'),
+    ip_address: document.querySelector('#ip-address'),
   },
   wakeLock: null,
   color: null,
 };
 
-lux.addEventListener('input', (event) => {
-  appOpts.dom.root.style.setProperty('--lightness', `${event.target.value}%`);
-});
+document.addEventListener('click', (event) => {
 
-color.addEventListener('input', (event) => {
-  changeBackgroundColor(event.target.value);
-});
+  // postData('http://'+ appOpts.dom.ip_address.value +':8060/keypress/'+ event.target.value)
+  //   .then(data => console.log(JSON.stringify(data))) // JSON-string from `response.json()` call
+  //   .catch(error => console.error(error));
 
-color.addEventListener('change', (event) => {
-  appOpts.color = event.target.value;
-  requestIdleCallback(storeBackgroundColor, {
-    timeout: 5000
-  });
-});
-
-const storeBackgroundColor = () => {
-  localStorage.setItem('color', appOpts.color);
-}
-
-const changeBackgroundColor = (color) => {
-  const [h, s, l] = rgbhexToHsl(color);
-  appOpts.dom.root.style.setProperty('--hue', h);
-  appOpts.dom.root.style.setProperty('--saturation', `${s}%`);
-  appOpts.dom.root.style.setProperty('--lightness', `${l}%`);
-  lux.value = l;
-}
-
-// I was originally going to just use the algo from
-// https://en.wikipedia.org/wiki/HSL_and_HSV but turns out friend of the show
-// @mjackson did it already in https://gist.github.com/mjackson/5311256, so I
-// just bolted a hex converter and some simple multipliers :-)
-const rgbhexToHsl = (color) => {
-  let [hex, r, g, b] = /([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})/i.exec(color);
-  r = parseInt(r, 16) / 255;
-  g = parseInt(g, 16) / 255;
-  b = parseInt(b, 16) / 255;
-
-  const max = Math.max(r, g, b);
-  const min = Math.min(r, g, b);
-  const l = Math.round(((max + min)) / 2 * 100);
-
-  let h, s;
-
-  if (max === min) {
-      h = s = 0;
-  } else {
-    const d = max - min;
-    s = Math.round((l > 0.5 ? d / (2 - max - min) : d / (max + min)) * 100);
-    switch(max) {
-      case r: h = (g - b) / d + (g < b ? 6 : 0); break;
-      case g: h = (b - r) / d + 2; break;
-      case b: h = (r - g) / d + 4; break;
-    }
-    h = Math.round((h / 6) * 360);
+  function postData(url = '', data = {}) {
+    // Default options are marked with *
+      return fetch(url, {
+          method: "POST", // *GET, POST, PUT, DELETE, etc.
+          mode: "no-cors", // no-cors, cors, *same-origin
+          cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+          credentials: "same-origin", // include, *same-origin, omit
+          headers: {
+              "Content-Type": "application/json",
+              // "Content-Type": "application/x-www-form-urlencoded",
+          },
+          redirect: "follow", // manual, *follow, error
+          referrer: "no-referrer", // no-referrer, *client
+          body: JSON.stringify(data), // body data type must match "Content-Type" header
+      })
+      .then(response => response.json()); // parses response to JSON
   }
-  return [h, s, l];
-}
+
+});
 
 const startWakeLock = () => {
   try {
@@ -86,13 +70,6 @@ const startServiceWorker = () => {
 }
 
 const init = () => {
-  const color = localStorage.getItem('color')
-  if (color) {
-    changeBackgroundColor(color);
-    appOpts.dom.color.value = color;
-  } else {
-    changeBackgroundColor('#ff69b4');
-  }
   startWakeLock();
   startServiceWorker();
 };
